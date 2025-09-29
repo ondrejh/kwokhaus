e531e16085539861ddc330032255a0b1f95f1bb8
Ondrej Hejda <ondrejh.ck@email.cz>
2025-09-29 11:57:35 +0200

    no sleep

--- fw/src/main.c
+++ fw/src/main.c
@@ -12,6 +12,8 @@
 #define TRIGGER_PIN 14
 #define SENSE_ADC_PIN 26
 
+#define millis() (to_ms_since_boot(get_absolute_time()))
+
 void init(void) {
   gpio_init(LED_GREEN_PIN);
   gpio_set_dir(LED_GREEN_PIN, GPIO_OUT);
@@ -26,14 +28,28 @@ int main() {
   init();
 
   bool led = false;
+  uint32_t tLed = 0;
 
-  sleep_ms(2000);           // Krátká prodleva, aby se terminál připojil
+  sleep_ms(100);
+
   while (true) {
-    sleep_ms(1000);       // případně další logika
-    printf("Hello World\r\n");
-    led = !led;
-    gpio_put(LED_GREEN_PIN, led);
-    gpio_put(TRIGGER_PIN, led);
-    put_pixel(urgb_u32(0x00, led ? 0x10 : 0x00, 0x00));
+    uint32_t now = millis();
+
+    if (led && ((now - tLed) >= 50)) {
+      led = false;
+      put_pixel(urgb_u32(0x00, 0x00, 0x00));
+    }
+    if (!led && ((now - tLed) > 1000)) {
+      led = true;
+      put_pixel(urgb_u32(0x00, 0x10, 0x00));
+      printf("Hello World\r\n");
+      tLed = now;
+    }
+
+    //led = !led;
+    //gpio_put(LED_GREEN_PIN, led);
+    //gpio_put(TRIGGER_PIN, led);
+    //put_pixel(urgb_u32(0x00, led ? 0x10 : 0x00, 0x00));
+    //sleep_ms(1000);
   }
 }
